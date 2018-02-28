0b58c0377f7790ef0c78d205a5b1128953e404c9
Adrian Kyzer <adriankyzer@gmail.com>
2018-02-27 20:06:33 -0500

    created files and entered various keys and tokens

---
+++ keys.js
@@ -0,0 +1,13 @@
+console.log('this is loaded');
+
+exports.twitter = {
+  consumer_key: process.env.QuJjyacTuN5Nau7e7XaTAIZ17,
+  consumer_secret: process.env.VcDNtOpfkdwxHEMIYk60lt6IkK4A4l2bxZlN2DpyJEAcZR3xdc,
+  access_token_key: process.env.968247402573463552-kSxSCPuWB7NdVGOQ13zoFvh4VU3eJ6o,
+  access_token_secret: process.env.kVfvVNXb5ovrCqQyOMwzceiGVRCyf21jFkPKPZMj2Sdau
+};
+
+exports.spotify = {
+  id: process.env.04dc4c7600634778927a641f82281e1c,
+  secret: process.env.d6ff9fadd73641f0b37260024c93e0c6
+};
