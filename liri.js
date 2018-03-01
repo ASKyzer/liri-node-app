ee8aaa98d50098952dce465eb04b52564d43211a
Adrian Kyzer <adriankyzer@gmail.com>
2018-03-01 06:06:14 -0500

    refactured the user enntry so that they don't have to use quotations in the song and movie name

--- liri.js
+++ liri.js
@@ -1,6 +1,6 @@
 require("dotenv").config(); // require the .env file
 
-var fs = require("fs");
+var fs = require("fs"); // built in file system to read and write files
 
 var keys = require("./keys.js"); // require the keys.js file
 // var spotify = new spotify(keys.spotify); // reads the spotify ids from the keys.js file
@@ -10,9 +10,20 @@ var client = new twitter(keys.twitter); // reads the twitter ids and tokens from
 var spotify = new spotify(keys.spotify);
 
 var userCommand = process.argv[2]; // assign the variable command to the user entry
-var nameOfSong = process.argv[3]; // name of the song that follows the 'spotify-this-song command'
-var nameOfMovie = process.argv[3]; // name of the movie that follows the movie-this command
-
+var nodeArgs = process.argv; // the array of user entry including the userCommand
+// create an empty string for holding the name of the song and name of movie
+var nameOfSong = "";
+var nameOfMovie = "";
+  // Capture all the words in the address (again ignoring the first two Node arguments)
+  for (var i = 3; i < nodeArgs.length; i++) {
+  // Build a string with the song entry.
+    nameOfSong = (nameOfSong + " " + nodeArgs[i]);
+    nameOfMovie = (nameOfMovie + "+" + nodeArgs[i]);
+  }// end of for loop to loop through process.argv
+  // the above loop returns a white space before the string so we need to get rid of that.
+  nameOfSong = nameOfSong.trim();
+  nameOfMovie = nameOfMovie.trim();
+console.log(nameOfMovie);
 // create a function that will read the user entry and see if it's one of the following: `my-tweets`,`spotify-this-song`,`movie-this`, or`do-what-it-says`
 var readCommand = function(){
   switch (userCommand) {
@@ -28,13 +39,10 @@ var readCommand = function(){
     case 'do-what-it-says': // if the user command is this
         getRandom(); // call the get random function to take the text inside of random.txt and then use it to call one of LIRI's commands.
       break;
-    default:
-
-      break;
-
-      console.log(userCommand);
+    // default:
+    //
+    //   break;
   } // end of switch/case
-
 } // end of the readCommand function
 
 // if the user command is 'my-tweets', create a function that will retrieve your latest 20 tweets and when they were created.
@@ -59,14 +67,9 @@ var getTweets = function(){
 
 } // end of getTweets function
 
-// if the user command is 'spotify-this-song + song name', create a function that will show the following information:
-    // song name,
-    // the artist(s),
-    // the album that song is from.
-    // preview link of the song from spotify and
-
+// since Spotify's API has the first letter of every word in their song name,
 // create a function to switch the first letter of the word in the string to uppercase
-function upperCase (nameOfSong){
+function upperCase(nameOfSong){
   return nameOfSong.toUpperCase();
 }
 // grab the first letter of each word of the string
@@ -74,15 +77,21 @@ function titleCase(nameOfSong){
 	var firstLetter = /(^|\s)[a-z]/g;
 	return nameOfSong.replace(firstLetter, upperCase);
 }
+console.log(nameOfSong); // check to see if the words in the song has capitalized first letters
 
+// if the user command is 'spotify-this-song + song name', create a function that will show the following information:
+    // song name,
+    // the artist(s),
+    // the album that song is from.
+    // preview link of the song from spotify
 var getSpotify = function(){
 
   var track;
-
-  if (nameOfSong != null){
+  // If the user entry is empty, then the default song is "Choose a Song".
+  if (process.argv[3] != null){
     track = titleCase(nameOfSong);
   } else {
-    track = "Yesterday"
+    track = "Choose a Song";
   }
 
     // per spoitfy's npm documentation to search their catalog via track name
@@ -111,7 +120,6 @@ var getSpotify = function(){
     else if (songsMatched.length === 0) {
       console.log("There are no songs that match your search.");
     }
-
     // create another loop to go through the tracks and get the info for each one
     for (var j =0; j < songsMatched.length; j++) {
       console.log("");
@@ -130,6 +138,7 @@ var getSpotify = function(){
   }); // end of search in spotify
 } // end of getSpotify function
 
+
 // if the user command is 'movie-this', create a function that will output the following information:
     // * Title of the movie.
     // * Year the movie came out.
@@ -143,13 +152,13 @@ var getMovie = function(){
 
   var movie;
   // If the user doesn't type a movie in, the program will output data for the movie 'Mr. Nobody.'
-  if (nameOfMovie != null){
+  if (process.argv[3] != null || process.argv[3] == ""){
     movie = nameOfMovie; // assign the user input to the movie var
   } else {
     movie = "Mr. Nobody";
   }
 
-
+  console.log(movie);
 
 } // end of getMovie function.
 
